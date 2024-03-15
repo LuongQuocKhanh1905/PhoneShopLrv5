93a16fc9591eba8eca8a4388ded6b3e74ad97983
hieptvh18 <hieptvh18@gmail.com>
2024-03-15 20:55:41 +0700

    first commit

---
+++ resources/lang/vn/footer.php
@@ -0,0 +1,14 @@
+<?php
+
+  return [
+    'AboutUs'          =>      'ກ່ຽວກັບພວກເຮົາ',
+    'address'          =>      ' Số 117 Nguyễn Viết Xuân, Vinh, Nghệ An.',
+    'FollowUs'         =>      'ຕິດຕາມພວກເຮົາ',
+    'visa'             =>      'Thanh toán bằng thẻ Visa.',
+    'mastercard'       =>      'Thanh toán bằng thẻ MasterCard.',
+    'jcb'              =>      'Thanh toán bằng thẻ JCB.',
+    'cod'              =>      'Thanh toán khi nhận hàng.',
+    'nganluong'        =>      'Giải pháp thanh toán qua Ngân Lượng.',
+    'payment'          =>      'ຊຳລະອອນລາຍ',
+    'support'          =>      'ນະໂຍບາຍ',
+  ];
---
+++ resources/lang/vn/header.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'Home'          =>       'ໜັ້າຫຼັກ',
+    'About'         =>       'ກ່ຽວກັບ',
+    'Products'      =>       'ສິນຄ້າ',
+    'News'          =>       'ຂ່າວສານ',
+    'Contact'       =>       'ຕິດຕໍ່',
+    'Login'         =>       'ເຂົ້າສູ່ລະບົບ',
+    'Logout'        =>       'Đăng Xuất',
+    'Register'      =>       'ລົງທະບຽນ',
+    'Search'        =>       'ຄົ້ນຫາ...',
+    'Producer'      =>       'Hãng Sản Xuất',
+
+];
